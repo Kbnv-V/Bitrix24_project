5ce7558a435084cd714971e586b84879b4d3fbd0
Medaza <115037299+Kbnv-V@users.noreply.github.com>
2025-12-11 16:41:04 +0300

    Update settings.php

--- get_element_list_id/settings.php
+++ get_element_list_id/settings.php
@@ -1,6 +1,6 @@
 <?php
-define('C_REST_CLIENT_ID','local.68b6d05a9ebd75.83126268');//Application ID
-define('C_REST_CLIENT_SECRET','48VZfg3xLpubFbL2gDseS1wbygRAk2OBzw2oi4hd82eO3qLIgf');//Application key
+define('C_REST_CLIENT_ID','ключ');//Application ID
+define('C_REST_CLIENT_SECRET','ключ');//Application key
 // or
 //define('C_REST_WEB_HOOK_URL','https://rest-api.bitrix24.com/rest/1/doutwqkjxgc3mgc1/');//url on creat Webhook
 
@@ -8,4 +8,5 @@ define('C_REST_CLIENT_SECRET','48VZfg3xLpubFbL2gDseS1wbygRAk2OBzw2oi4hd82eO3qLIg
 //define('C_REST_IGNORE_SSL',true);//turn off validate ssl by curl
 //define('C_REST_LOG_TYPE_DUMP',true); //logs save var_export for viewing convenience
 //define('C_REST_BLOCK_LOG',true);//turn off default logs
+
 //define('C_REST_LOGS_DIR', __DIR__ .'/logs/'); //directory path to save the log
